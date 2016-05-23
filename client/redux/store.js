f757a4ea8e85ec31f66bef2691bb12ce6b3e7bf8
Bryan Bierce <bryanbierce@gmail.com>
2016-05-22 20:49:48 -0700

    Added redux-thunk as middleware

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,14 +1,16 @@
-import { createStore, compose } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import reducer from './reducers';
 import { Map } from 'immutable';
 
 
 const initialState = new Map();
 
-const store = createStore(reducer, initialState, compose(
-  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
-    ? window.devToolsExtension()
-    : (f) => f
-));
-
-export default store;
+export default createStore(
+  reducer,
+  initialState,
+  compose(
+    applyMiddleware(ReduxThunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
